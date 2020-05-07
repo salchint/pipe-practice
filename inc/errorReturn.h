89d4a286191b932c3a8bd432b3f447b5004090d0
Thomas Strunz <thomas.strunz@zoho.com>
2020-05-07 16:21:46 -0600

    Add parameter verification to player

---
+++ inc/errorReturn.h
@@ -0,0 +1,40 @@
+/*
+ *errorReturn.h
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+ /*
+  *Error codes used upon exiting the program.
+  */
+enum PlayerErrorCodes {
+    E_INVALID_ARGS_COUNT = 1,
+    E_INVALID_PLAYER_COUNT = 2,
+    E_INVALID_PLAYER_ID = 3,
+    E_INVALID_PATH = 4,
+    E_EARLY_GAME_OVER = 5,
+    E_COMMS_ERROR = 6
+};
+
+/*
+ *Error messages sent to stderr.
+ */
+const char* PlayerErrorTexts[] = {
+    "",
+    "Usage: player pcount ID",
+    "Invalid player count",
+    "Invalid ID",
+    "Invalid path",
+    "Early game over",
+    "Communications error"
+};
+
+/*
+ *Print an error message to stderr and exit the program.
+ */
+void errorReturn(FILE* destination, enum PlayerErrorCodes code) {
+    fprintf(destination, "%s\n", PlayerErrorTexts[code]);
+    exit(code);
+}
+
--- src-2310A/main.c
+++ src-2310A/main.c
@@ -1,9 +1,42 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+/*#include <unistd.h>*/
+#include <string.h>
+#include <ctype.h>
+#include "../inc/errorReturn.h"
 
-int main(int argc, char** argv)
-{
+int main(int argc, char* argv[]) {
+    int playersCount = 0;
+    int playerID = 0;
+    int i = 0;
 
-  return 0;
+    /*Check for valid number of parameters*/
+    if (3 != argc) {
+        errorReturn(stderr, E_INVALID_ARGS_COUNT);
+    }
+
+    /*Check for valid number of players*/
+    for (i = 0; i < strlen(argv[1]); i++) {
+        if (!isdigit(argv[1][i])) {
+            errorReturn(stderr, E_INVALID_PLAYER_COUNT);
+        }
+    }
+    playersCount = atoi(argv[1]);
+    if (1 > playersCount) {
+        errorReturn(stderr, E_INVALID_PLAYER_COUNT);
+    }
+
+    /*Check for valid player ID*/
+    for (i = 0; i < strlen(argv[2]); i++) {
+        if (!isdigit(argv[2][i])) {
+            errorReturn(stderr, E_INVALID_PLAYER_ID);
+        }
+    }
+    playerID = atoi(argv[2]);
+    if (playersCount <= playerID) {
+        errorReturn(stderr, E_INVALID_PLAYER_ID);
+    }
+
+    return EXIT_SUCCESS;
 }
+
